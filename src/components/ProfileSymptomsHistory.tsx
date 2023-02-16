098038075f207680f86e44d7afc6b3f8ab3b0b42
Matteo Delfino <matteo.delfino@isi.it>
2023-02-16 17:47:21 +0100

    format components code with prettier

--- src/components/ProfileSymptomsHistory.tsx
+++ src/components/ProfileSymptomsHistory.tsx
@@ -1,49 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import { studyAPI } from 'case-web-app-core';
+import React, { useEffect, useState } from "react";
+import { studyAPI } from "case-web-app-core";
 
 interface ProfileSymptomsHistoryProps {
-  studyId: string,
-  profileId: string
+  studyId: string;
+  profileId: string;
 }
 
 type ReportRequestParameters = Parameters<typeof studyAPI.getReportsForUser>;
 
 const ProfileSymptomsHistory: React.FC<ProfileSymptomsHistoryProps> = (props) => {
-
   const [index, setIndex] = useState(0);
   const [reports, setReports] = useState([]); // add type
-  const prev = function() {
+  const prev = function () {
     throw new Error();
   };
-  const next = function() {
+  const next = function () {
     throw new Error();
   };
 
   useEffect(() => {
-
     const getSymptoms = async (startingDate: number | undefined = undefined) => {
-
-    const requestParameters: ReportRequestParameters = [
-      [props.studyId],
-      [props.profileId],
-      "symptomsFeedback", // read from shared model
-      undefined,
-      startingDate,
-      undefined,
-      5
-    ];
+      const requestParameters: ReportRequestParameters = [
+        [props.studyId],
+        [props.profileId],
+        "symptomsFeedback", // read from shared model
+        undefined,
+        startingDate,
+        undefined,
+        5,
+      ];
 
       try {
         const response = await studyAPI.getReportsForUser(...requestParameters);
 
         console.log(response);
+      } catch {
+        /* empty */
+      } finally {
+        /* empty */
       }
-      catch { /* empty */ }
-      finally { /* empty */ }
     };
 
     getSymptoms();
-
   }, [props.studyId, props.profileId]);
 
   return <></>;
--- src/components/UserProfilesSelector.tsx
+++ src/components/UserProfilesSelector.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
 interface UserProfilesSelectorProps {
-  selectedProfileId: string
+  selectedProfileId: string;
 }
 
 const UserProfilesSelector: React.FC<UserProfilesSelectorProps> = (props) => {
-
   // set the correct type but it must be exported case web app core
   const profiles = useSelector((state: any) => state.user.currentUser.profiles);
 
-  return (
-    <></>
-  );
+  return <></>;
 };
 
 export default UserProfilesSelector;
-
