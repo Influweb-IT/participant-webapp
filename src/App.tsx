0ab94413e630d96458d5612ef93784b4f34f6dbb
Matteo Delfino <matteo.delfino@isi.it>
2023-03-17 10:26:26 +0100

    create extension for UserSymptomsHistory component

--- src/App.tsx
+++ src/App.tsx
@@ -41,6 +41,9 @@ const App: React.FC = () => {
         pagesConfig={pagesConfig as PagesConfig}
         footerConfig={footerConfig as FooterConfig}
         dateLocales={dateLocales}
+        extensions={[{ "name": "userSymptomsHistory",
+                       "component": UserSymptomsHistory}
+                    ]}
       />
       <Iubenda />
       <Facebook />
