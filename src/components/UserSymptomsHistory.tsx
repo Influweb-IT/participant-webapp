06d0e6b3a07337c78d662c1c852d1fd8cb662d1c
Matteo Delfino <matteo.delfino@isi.it>
2023-03-22 11:36:01 +0100

    symptoms history: jsx implementation first stub

--- src/components/UserSymptomsHistory.tsx
+++ src/components/UserSymptomsHistory.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import ProfileSymptomsHistory from "./ProfileSymptomsHistory";
 import UserProfilesSelector from "./UserProfilesSelector";
 
-const UserSymptomsHistory: React.FC = () => {
-  // set to the main profile id at first render
-  const [selectedProfileId, setSelectedProfileId] = useState("");
+interface UserSymptomsHistoryProps {
+  className?: string;
+}
+
+const UserSymptomsHistory: React.FC<UserSymptomsHistoryProps> = (props) => {
+  const profiles: Array<any> = useSelector((state: any) => state.user.currentUser.profiles);
+  const mainProfileId = profiles.filter((profile) => profile.mainProfile)[ 0 ].alias;
+  const studyId = "influweb";
+
+  const [ selectedProfileId, setSelectedProfileId ] = useState(mainProfileId);
 
   return (
-    <>
-      <UserProfilesSelector selectedProfileId={selectedProfileId} />
-      <ProfileSymptomsHistory studyId="influweb" profileId={selectedProfileId} />
-    </>
+    <div className={props.className}>
+      <UserProfilesSelector selectedProfileId={selectedProfileId} onProfileChange={setSelectedProfileId} />
+      <ProfileSymptomsHistory studyId={studyId} profileId={selectedProfileId} />
+    </div>
   );
 };
+
+UserSymptomsHistory.defaultProps = {
+  className: "row g-0 bg-primary"
+}
 
 export default UserSymptomsHistory;
