e64ff1574816f74835e49c3d1b3f3e3f3a2a010b
gf-isi <104016673+gf-isi@users.noreply.github.com>
2026-05-21 09:07:03 +0200

    moved from cra to vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store, initI18n } from "@influenzanet/case-web-app-core";
 
@@ -41,7 +40,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
